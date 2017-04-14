bfa6e42cd9371741bba35d68a0f467e0fe698b0b
Archi <it.avpatel@gmail.com>
2017-04-14 10:16:25 -0700

    Comment Color

--- js/libs/d3.v2.js
+++ js/libs/d3.v2.js
@@ -3034,18 +3034,18 @@ var d3_category10 = [
         "#bcbd22", "#dbdb8d"
     ];
 
-    var d3_category20 = [
-        "#1f77b4", "#aec7e8",
-        "#ff7f0e", "#ffbb78",
-        "#2ca02c", "#98df8a",
-        "#d62728", "#ff9896",
-        "#9467bd", "#c5b0d5",
-        "#8c564b", "#c49c94",
-        "#e377c2", "#f7b6d2",
-        "#7f7f7f", "#c7c7c7",
-        "#bcbd22", "#dbdb8d",
-        "#17becf", "#9edae5"
-    ];
+    // var d3_category20 = [
+    //     "#1f77b4", "#aec7e8",
+    //     "#ff7f0e", "#ffbb78",
+    //     "#2ca02c", "#98df8a",
+    //     "#d62728", "#ff9896",
+    //     "#9467bd", "#c5b0d5",
+    //     "#8c564b", "#c49c94",
+    //     "#e377c2", "#f7b6d2",
+    //     "#7f7f7f", "#c7c7c7",
+    //     "#bcbd22", "#dbdb8d",
+    //     "#17becf", "#9edae5"
+    // ];
 
 
 
